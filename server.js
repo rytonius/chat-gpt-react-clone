dbd6071539bdf61905e54dcc86dffb6f13977c90
RyanGaming <57922617+LMJ-Ryan@users.noreply.github.com>
2023-05-07 23:22:05 -0800

    finish the majority of it

--- server.js
+++ server.js
@@ -7,30 +7,36 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
-//const API_KEY = ''
+const API_KEY = ''
 
 app.post('/completions', async (req, res) => {
-    const options = {
-        method: "POST",
-        headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            'Content-Type': "application/json"
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: req.body.messages }],
-            max_tokens: 50,
-        })
-
-    };
-
-    try {
-        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
-        const modelData = await response.json();
-        res.send(modelData);
-    } catch (error) {
-        console.error(error)
+    if (API_KEY === '') {
+        console.log("API KEY IS BLANK ON SERVER.JS line 10")
     }
+    else {
+        const options = {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${API_KEY}`,
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{ role: "user", content: req.body.messages }],
+                max_tokens: 50,
+            })
+    
+        };
+    
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+            const modelData = await response.json();
+            res.send(modelData);
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
 });
 
 app.listen(PORT, () => console.log(`  Listening on http://127.0.0.1:${PORT}`));
--- src/App.jsx
+++ src/App.jsx
@@ -4,64 +4,111 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 const App = () => {
-    const [count, setCount] = useState(0);
     const [value, setValue] = useState("");
     const [message, setMessage] = useState(null);
-    const [previousChats, setPreviousChats] = useState(null);
+    const [previousChats, setPreviousChats] = useState([]);
+    const [currentTitle, setCurrentTitle] = useState(null);
+    const [currentModel, setCurrentModel] = useState(null);
+
+    const createNewChat = () => {
+        setMessage(null);
+        setCurrentTitle(null);
+        setValue("");
+        setCurrentModel(null);
+    }
+
+    const handleClick = (uniqueTitle) => {
+        setCurrentTitle(uniqueTitle)
+    }
 
     const getMessages = async () => {
-
-        const options = {
-            method: "POST",
-            body: JSON.stringify({
-                messages: value
-            }),
-            headers: {
-                'Content-Type': "application/json"
+        if (value !== "" && !!(value.trim())) {
+            const options = {
+                method: "POST",
+                body: JSON.stringify({
+                    messages: value
+                }),
+                headers: {
+                    'Content-Type': "application/json"
+                }
+            };
+            try {
+                const response = await fetch('http://127.0.0.1:9001/completions', options);
+                const data =  await response.json();
+                console.log(data);
+                setCurrentModel(data.model);
+                setMessage(data.choices[0].message)
+    
+            } catch (error) {
+                console.log(error);
             }
-        };
-        try {
-            const response = await fetch('http://127.0.0.1:9001/completions', options);
-            const data =  await response.json();
-            console.log(data);
-            setMessage(data.choices[0].message)
-
-        } catch (error) {
-            console.log(error);
         }
     }
+
+    useEffect(() => {
+        //console.log(currentTitle, currentModel, value, message);
+        if (!currentTitle && value && message) {
+            setCurrentTitle(value);
+        }
+        if (currentTitle && value && message) {
+            setPreviousChats(prevChats => (
+                [...prevChats, {
+                    title: currentTitle,
+                    role: "user",
+                    content: value
+                }, {
+                    title: currentTitle,
+                    role: currentModel ? currentModel + " : " + message.role : message.role,
+                    content: message.content,
+                    //model: 
+                }]
+            ))
+        }
+    }, [message] )
 
     useEffect(() => {
         for (const mybuttons of document.querySelectorAll("button")) {
             mybuttons.onmousemove = e => handleOnMouseMove(e);
         }
     })    
+
+    const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle)
+    console.log(currentTitle);
+    const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)));
+
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            getMessages();
+        }
+    }
+
     return (
     <>
+    
         <div className="app">
             <section className="side-bar">
-                <button>＋ New Chat</button>
+                <button onClick={createNewChat}>＋ New Chat</button>
                 <ul className="history">
-                    <li>hello</li>
-                    <li>hello</li>
-                    <li>hello</li>
+                    {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
                 </ul>
                 <nav>
                     <h1>Handle with care</h1>
                 </nav>
             </section>
             <section className="main">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-
                 <h1>RytoniusGTP</h1>
                 <ul className="feed">
-
+                    {currentChat?.map((chatMessage, index) => <li key={index}>
+                        <p className="role">{chatMessage.role}</p>
+                        <p>{chatMessage.content}</p>
+                        
+                        </li>)}
                 </ul>
                 <div className="bottom-section">
                     <div className="input-container">
-                        <input value={value} onChange={(e) => setValue(e.target.value)}/>
+                        <input  value={value} 
+                                onChange={(e) => setValue(e.target.value)}
+                                onKeyDown={handleEnter}/>
                         <div id="submit" onClick={getMessages}>ॵ</div>
 
                     </div>
